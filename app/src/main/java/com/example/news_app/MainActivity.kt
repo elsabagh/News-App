1dcc9d06e016b0230300d6f67fc9f070fe3f462d
elsabagh <elsabagha025@gmail.com>
2024-09-25 12:24:52 +0300

    create Dagger Hilt

--- app/src/main/java/com/example/news_app/MainActivity.kt
+++ app/src/main/java/com/example/news_app/MainActivity.kt
@@ -1,27 +1,36 @@
 package com.example.news_app
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import com.example.news_app.domain.usecases.appentry.AppEntryUseCase
 import com.example.news_app.presentation.onBoarding.OnBoardingScreen
 import com.example.news_app.ui.theme.NewsAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @Inject
+    lateinit var appEntryUseCase: AppEntryUseCase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
+        lifecycleScope.launch {
+            appEntryUseCase.readAppEntry().collect {
+                Log.d("Test", it.toString())
+            }
+        }
         setContent {
             NewsAppTheme {
                 Box(
---
+++ app/src/main/java/com/example/news_app/di/AppModule.kt
@@ -0,0 +1,35 @@
+package com.example.news_app.di
+
+import android.app.Application
+import com.example.news_app.data.manger.LocalUserMangerImpl
+import com.example.news_app.domain.manger.LocalUserManger
+import com.example.news_app.domain.usecases.appentry.AppEntryUseCase
+import com.example.news_app.domain.usecases.appentry.RadAppEntry
+import com.example.news_app.domain.usecases.appentry.SaveAppEntry
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideLocalUserManger(
+        application: Application
+    ): LocalUserManger {
+        return LocalUserMangerImpl(application)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAppEntryUseCase(
+        localUserManger: LocalUserManger
+    ) = AppEntryUseCase(
+        readAppEntry = RadAppEntry(localUserManger),
+        saveAppEntry = SaveAppEntry(localUserManger)
+    )
+}
